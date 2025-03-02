c5ded7eded3b82654cdef17dec5a2c2989e09213
Hitesh203 <hiteshkumarmz16@gmail.com>
2025-03-02 21:59:36 +0530

    Added Merge solution

---
+++ March/Merge2d.py
@@ -0,0 +1,32 @@
+#2570.Merge Two 2D Arrays by Summing Values Mar-02/2025
+class Solution(object):
+    def mergeArrays(self, nums1, nums2):
+        """
+        :type nums1: List[List[int]]
+        :type nums2: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        i, j = 0, 0
+        merged = []
+
+        while i < len(nums1) and j < len(nums2):
+            if nums1[i][0] == nums2[j][0]:
+                merged.append([nums1[i][0], nums1[i][1] + nums2[j][1]])
+                i += 1
+                j += 1
+            elif nums1[i][0] < nums2[j][0]:
+                merged.append(nums1[i])
+                i += 1
+            else:
+                merged.append(nums2[j])
+                j += 1
+
+        while i < len(nums1):
+            merged.append(nums1[i])
+            i += 1
+        
+        while j < len(nums2):
+            merged.append(nums2[j])
+            j += 1
+
+        return merged
